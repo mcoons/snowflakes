c3ad3136298108a62a9d71e568d797c3c6286e71
mcoons <mcoons67@gmail.com>
2019-01-24 01:13:10 -0700

    added credit for snowflakes

--- snowflakes.js
+++ snowflakes.js
@@ -8,29 +8,13 @@ document.body.insertBefore(canvas, document.body.firstChild);
 var ctx = canvas.getContext('2d');
 var canvasRect = canvas.getBoundingClientRect();
 
-// window.addEventListener("mousemove", mouseMove);
 window.addEventListener('resize', resizeCanvas, false);
 window.setInterval(loop, 17);
 window.setInterval(windChange, 10000);
 
 var snowflakes = [];
-
 var windDirection = 0;
 var deltaXChange = .001;
-
-// var mouse = {
-//     lastX: null,
-//     lastY: null,
-//     lastTime: null,
-//     currentX: null,
-//     currentY: null,
-//     currentTime: null
-// };
-// var mouseInfluence = 100;
-
-var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
-grd.addColorStop(0, "red");
-grd.addColorStop(1, "white");
 
 function loop() {
     if (snowflakes.length < canvas.width / 2 && !(Date().now % 3))
@@ -57,18 +41,15 @@ function initializeSnowflake(flake) {
     return flake;
 }
 
+function lerp (start, end, amt){
+    return (1-amt)*start+amt*end
+}
+
 function updateSnowflakes() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     snowflakes.forEach(flake => {
-
-        if (flake.speedX > windDirection) {
-            flake.speedX += deltaXChange*windDirection;
-        }
-
-        if (flake.speedX < windDirection) {
-            flake.speedX += deltaXChange*windDirection;
-        }
+        flake.speedX = lerp(flake.speedX, windDirection, deltaXChange);
 
         flake.y += flake.speedY;
         flake.x += flake.speedX;
@@ -81,18 +62,9 @@ function updateSnowflakes() {
             initializeSnowflake(flake);
 
         if (flake.x > 0 && flake.x < canvas.width) 
-        drawSnowflake(flake);
+            drawSnowflake(flake);
     });
-
-    // grd = ctx.createRadialGradient(mouse.currentX, mouse.currentY, 10, mouse.currentX, mouse.currentY, mouseInfluence);
-    // grd.addColorStop(0, "rgba(255,0,0,.9)");
-    // grd.addColorStop(1, "rgba(255,0,0,.01)");
-
-    // ctx.beginPath();
-    // ctx.arc(mouse.currentX, mouse.currentY, mouseInfluence, 0, Math.PI*2, false);
-    // ctx.fillStyle = grd;
-    // ctx.fill();
-  
+    // console.log(snowflakes);
 }
 
 function drawSnowflake(flake) {
@@ -110,31 +82,7 @@ function resizeCanvas() {
     canvasRect = canvas.getBoundingClientRect();
 }
 
-// function mouseMove(event){
-
-//     mouse.lastTime = mouse.currentTime;
-//     mouse.lastX = mouse.currentX;
-//     mouse.lastY = mouse.currentY;
-
-//     mouse.currentX = event.clientX - canvasRect.left;
-//     mouse.currentY = event.clientY - canvasRect.top;
-//     mouse.currentTime = new Date().getTime();
-
-//     // console.log(mouse);
-// }
-
 function windChange(){
     windDirection = Math.round(Math.random()*20 - 10);
-    console.log("wind Change", windDirection);
-
-
-    // let chance = Math.round(Math.random()*10 - 5);
-    // if (chance < -2){
-    //     console.log("left", chance)
-    // } else
-    // if (chance > 2){
-    //     console.log("right", chance)
-    // } else {
-    //     console.log("none", chance)
-    // }
+    // console.log("wind Change", windDirection);
 }
