b7beb19c566b2dae1ff3930d2b62f29d27825afc
mcoons <mcoons67@gmail.com>
2019-01-24 14:01:19 -0700

    refactoring

--- snowflakes.js
+++ snowflakes.js
@@ -9,15 +9,17 @@ var ctx = canvas.getContext('2d');
 var canvasRect = canvas.getBoundingClientRect();
 
 window.addEventListener('resize', resizeCanvas, false);
-window.setInterval(loop, 17);
+window.setInterval(loop, 30);
 window.setInterval(windChange, 10000);
+window.setInterval(accumulationChange, 20000);
 
 var snowflakes = [];
 var windDirection = 0;
 var deltaXChange = .001;
+var maxFlakes = canvas.width/10;
 
 function loop() {
-    if (snowflakes.length < canvas.width / 2 && !(Date().now % 3))
+    if (snowflakes.length < maxFlakes && !(Date().now % 3))
         createSnowflake();
     updateSnowflakes();
 }
@@ -28,7 +30,8 @@ function createSnowflake() {
 
 function initializeSnowflake(flake) {
     let size = Math.floor(5 + Math.random() * 25);
-    flake.x = Math.floor(Math.random() * canvas.width * 3 - canvas.width);
+    // flake.x = Math.floor(Math.random() * canvas.width * 3 - canvas.width);
+    flake.x = Math.floor(Math.random() * canvas.width);
     flake.y = -100;
     flake.rotation = 0;
     flake.rotationSpeed = (Math.round(Math.random() * 10) - 5) / 300;
@@ -41,8 +44,8 @@ function initializeSnowflake(flake) {
     return flake;
 }
 
-function lerp (start, end, amt){
-    return (1-amt)*start+amt*end
+function lerp(start, end, amt) {
+    return (1 - amt) * start + amt * end;
 }
 
 function updateSnowflakes() {
@@ -50,18 +53,33 @@ function updateSnowflakes() {
 
     snowflakes.forEach(flake => {
         flake.speedX = lerp(flake.speedX, windDirection, deltaXChange);
-
         flake.y += flake.speedY;
         flake.x += flake.speedX;
         flake.rotation += flake.rotationSpeed;
 
-        if (Math.abs(flake.rotation) > 2 * Math.PI)
-            flake.rotation = 0;
+        flake.rotation = (Math.abs(flake.rotation) > 2 * Math.PI) ? 0 : flake.rotation;
+        // if (Math.abs(flake.rotation) > 2 * Math.PI)
+        //     flake.rotation = 0;
 
-        if (flake.y > canvas.height || flake.x > canvas.width*2 || flake.x < -canvas.width)
+        // if (flake.y > canvas.height || flake.x > canvas.width * 2 || flake.x < -canvas.width)
+        //     initializeSnowflake(flake);
+
+        if (flake.y > canvas.height)
             initializeSnowflake(flake);
 
-        if (flake.x > 0 && flake.x < canvas.width) 
+        // if (flake.y > canvas.height)
+        //     flake.y = -100;
+
+        flake.x = (flake.x > canvas.width + 51) ? -50 : flake.x;
+        // if (flake.x > canvas.width + 51)
+        //     flake.x = -50;
+
+        flake.x = (flake.x < -51) ? canvas.width + 50 : flake.x;
+        // if (flake.x < -51)
+        //     flake.x = canvas.width + 50;
+
+
+        if (flake.x > 0 && flake.x < canvas.width)
             drawSnowflake(flake);
     });
     // console.log(snowflakes);
@@ -82,7 +100,11 @@ function resizeCanvas() {
     canvasRect = canvas.getBoundingClientRect();
 }
 
-function windChange(){
-    windDirection = Math.round(Math.random()*20 - 10);
+function windChange() {
+    windDirection = Math.round(Math.random() * 20 - 10);
     // console.log("wind Change", windDirection);
+}
+
+function accumulationChange() {
+
 }
